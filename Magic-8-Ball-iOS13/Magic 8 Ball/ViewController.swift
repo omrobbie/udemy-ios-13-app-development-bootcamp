f07e0540fbe96d3edb34f9f8e018bef77aad298a
omrobbie <putu.roby@yahoo.com>
2020-06-17 15:25:52 +0800

    connect storyboard to code and give it random answer

--- Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -10,5 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imgAnswer: UIImageView!
+
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+
+    @IBAction func btnAnswerTapped(_ sender: Any) {
+        imgAnswer.image = ballArray.randomElement()
+    }
 }

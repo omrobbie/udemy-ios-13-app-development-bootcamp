4e1999c5c9d43077fae9f2eb78adb65320150e65
omrobbie <putu.roby@yahoo.com>
2020-06-18 07:18:27 +0800

    refactor: simplify the code

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -11,26 +11,16 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    var player: AVAudioPlayer?
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     func playSound() {
-        guard let url = Bundle.main.url(forResource: "C", withExtension: ".wav") else {return}
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-            guard let player = player else {return}
-            player.play()
-        } catch {
-            print(error.localizedDescription)
-            return
-        }
+        let url = Bundle.main.url(forResource: "C", withExtension: ".wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 
     @IBAction func btnKeyTapped(_ sender: UIButton) {

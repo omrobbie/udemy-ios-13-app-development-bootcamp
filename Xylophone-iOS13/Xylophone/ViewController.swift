ecdc002349f0939217b101ff5668e22de8bc35f1
omrobbie <putu.roby@yahoo.com>
2020-06-18 07:12:03 +0800

    playing sound for c tone

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -7,10 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: ".wav") else {return}
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = player else {return}
+            player.play()
+        } catch {
+            print(error.localizedDescription)
+            return
+        }
+    }
+
+    @IBAction func btnKeyTapped(_ sender: UIButton) {
+        playSound()
     }
 }

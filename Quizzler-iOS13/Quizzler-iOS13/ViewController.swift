1406717a142abca8eabb9c20e1fb3931cc597d41
omrobbie <putu.roby@yahoo.com>
2020-06-18 11:36:37 +0800

    connect layout to view controller

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var lblQuestion: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var btnTrue: UIButton!
+    @IBOutlet weak var btnFalse: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func btnAnswerTapped(_ sender: UIButton) {
+        
     }
 }

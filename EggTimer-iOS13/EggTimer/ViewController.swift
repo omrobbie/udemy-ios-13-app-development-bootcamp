14d886c2cb6d8fa81fef22fe9bb6af4e49cd1a37
omrobbie <putu.roby@yahoo.com>
2020-06-18 11:05:17 +0800

    setup timer and update the title

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,10 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    @IBOutlet weak var lblTitle: UILabel!
+
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 5]
+
+    var secondsRemaining = 60
+    var timer = Timer()
+
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            lblTitle.text = "DONE!"
+        }
+    }
 
     @IBAction func btnHardnessTapped(_ sender: UIButton) {
         let hardness = sender.currentTitle!
-        print(eggTimes[hardness]!)
+        secondsRemaining = eggTimes[hardness]!
+
+        timer.invalidate()
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
 }

6c6eea18279ac55604df9eaa1b27ff939564fed1
omrobbie <putu.roby@yahoo.com>
2020-06-18 16:56:06 +0800

    update story label and button titles statically

--- Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+
+    let story = [
+        "You see a fork in the road."
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        storyLabel.text = story[0]
+        choice1Button.setTitle("Take a left.", for: .normal)
+        choice2Button.setTitle("Take a right.", for: .normal)
     }
 }

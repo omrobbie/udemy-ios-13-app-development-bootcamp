a687ac0f546b03af145efb193bc9d418942774cb
omrobbie <putu.roby@yahoo.com>
2020-06-18 17:12:01 +0800

    refactor: change app structure to MVC

--- Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -14,13 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
 
-    let story = [
-        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
-        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
-    ]
-
-    var storyNumber = 0
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +22,13 @@ class ViewController: UIViewController {
     }
 
     func updateUI() {
-        storyLabel.text = story[storyNumber].title
-        choice1Button.setTitle(story[storyNumber].choice1, for: .normal)
-        choice2Button.setTitle(story[storyNumber].choice2, for: .normal)
+        storyLabel.text = storyBrain.getStory()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
 
     @IBAction func btnAnswerTapped(_ sender: UIButton) {
-        storyNumber += 1
+        storyBrain.nextStory()
         updateUI()
     }
 }
--- Destini-iOS13/Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Destini-iOS13/Model/StoryBrain.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+struct StoryBrain {
+
+    private let story = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+
+    private var storyNumber = 0
+
+    func getStory() -> String {
+        return story[storyNumber].title
+    }
+
+    func getChoice1() -> String {
+        return story[storyNumber].choice1
+    }
+
+    func getChoice2() -> String {
+        return story[storyNumber].choice2
+    }
+
+    mutating func nextStory() {
+        if storyNumber < story.count - 1 {
+            storyNumber += 1
+        } else {
+            storyNumber = 0
+        }
+    }
+}

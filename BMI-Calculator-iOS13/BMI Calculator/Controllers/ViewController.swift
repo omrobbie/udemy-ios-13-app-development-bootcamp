a9ff6624f75b4c51b0186b56f9a053dd21e59802
omrobbie <putu.roby@yahoo.com>
2020-06-18 18:04:21 +0800

    connect slider and label to view controller

--- BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/ViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblHeight: UILabel!
+    @IBOutlet weak var lblWeight: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func sliderHeightChanged(_ sender: UISlider) {
+        let value = String(format: "%.2f", sender.value)
+        lblHeight.text = "\(value)m"
+    }
+
+    @IBAction func sliderWeightChanged(_ sender: UISlider) {
+        let value = String(format: "%.0f", sender.value)
+        lblWeight.text = "\(value)Kg"
     }
 }

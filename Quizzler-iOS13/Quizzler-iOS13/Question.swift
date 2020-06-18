f4795f9bf50a7c722c94ca4cf4e8715290ccdea9
omrobbie <putu.roby@yahoo.com>
2020-06-18 12:06:30 +0800

    refactor: update data using struct

---
+++ Quizzler-iOS13/Quizzler-iOS13/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by omrobbie on 18/06/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+
+    let question: String
+    let answer: String
+}
--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -14,40 +14,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var btnTrue: UIButton!
     @IBOutlet weak var btnFalse: UIButton!
-
+    
     let quiz = [
-        ["Four + Two is equal to Six", "True"],
-        ["Five - Three is greater than One", "True"],
-        ["Three + Eight is less than Ten", "False"],
+        Question(question: "Four + Two is equal to Six", answer: "True"),
+        Question(question: "Five - Three is greater than One", answer: "True"),
+        Question(question: "Three + Eight is less than Ten", answer: "False"),
     ]
-
+    
     var questionNumber = 0
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
-
+    
     func updateUI() {
-        lblQuestion.text = quiz[questionNumber][0]
+        lblQuestion.text = quiz[questionNumber].question
     }
-
+    
     @IBAction func btnAnswerTapped(_ sender: UIButton) {
         let userAnswer = sender.currentTitle!
-        let actualAnswer = quiz[questionNumber][1]
-
+        let actualAnswer = quiz[questionNumber].answer
+        
         if userAnswer == actualAnswer {
             print("Right!")
         } else {
             print("Wrong!")
         }
-
+        
         if questionNumber < quiz.count - 1 {
             questionNumber += 1
         } else {
             questionNumber = 0
         }
-
+        
         updateUI()
     }
 }

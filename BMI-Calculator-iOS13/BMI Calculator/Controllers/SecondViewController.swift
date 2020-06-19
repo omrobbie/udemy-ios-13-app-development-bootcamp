f430b9150ad0ab67fa7e049ce74078b929f3cd60
omrobbie <putu.roby@yahoo.com>
2020-06-19 10:47:16 +0800

    adding second view controller

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by omrobbie on 19/06/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

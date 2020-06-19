837960fb2e3ca76aba55cefad433c1fe4fa49f5d
omrobbie <putu.roby@yahoo.com>
2020-06-19 12:11:57 +0800

    refactor: create bmi data model

---
+++ BMI-Calculator-iOS13/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by omrobbie on 19/06/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
--- BMI-Calculator-iOS13/BMI Calculator/Models/CalculatorBrain.swift
+++ BMI-Calculator-iOS13/BMI Calculator/Models/CalculatorBrain.swift
@@ -10,13 +10,21 @@ import Foundation
 
 struct CalculatorBrain {
 
-    var bmi: Float = 0.0
+    var bmi: BMI?
 
     mutating func calculatorBMI(height: Float, weight: Float)  {
-        bmi = weight / pow(height, 2)
+        let bmiValue = weight / pow(height, 2)
+
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))
+        }
     }
 
     func getBMIValue() -> String {
-        return String(format: "%.1f", bmi)
+        return String(format: "%.1f", bmi?.value ?? 0.0)
     }
 }

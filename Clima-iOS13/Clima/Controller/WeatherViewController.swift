c7c53975b2d369b3421abbb492131d1906e01cbc
omrobbie <putu.roby@yahoo.com>
2020-06-19 14:44:03 +0800

    improve search text field

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -13,8 +13,38 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
-    
+    @IBOutlet weak var searchTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTextField.delegate = self
+    }
+
+    @IBAction func btnSearchTapped(_ sender: Any) {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+}
+
+extension WeatherViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something..."
+            return false
+        }
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+        searchTextField.placeholder = "Search"
     }
 }

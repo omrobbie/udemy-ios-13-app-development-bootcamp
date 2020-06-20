cf34f6f19da2d5f1224623b4cb4dc151f8a4ffcd
omrobbie <putu.roby@yahoo.com>
2020-06-20 10:32:03 +0800

    create temperature into string format

--- Clima-iOS13/Clima/Model/WeatherManager.swift
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -43,6 +43,7 @@ struct WeatherManager {
 
             let weather = WeatherModel(conditionId: weatherId, cityName: name, temperature: temp)
             print(weather.conditionName)
+            print(weather.temperatureString)
         } catch {
             print(error.localizedDescription)
         }

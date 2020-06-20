ba0ccd72e7b64f0c25d65d95159ce340e8870912
omrobbie <putu.roby@yahoo.com>
2020-06-20 12:27:04 +0800

    display all currency array to picker

--- ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
+++ ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -14,7 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblCurrency: UILabel!
     @IBOutlet weak var pckCurrency: UIPickerView!
 
+    let coinManager = CoinManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        pckCurrency.delegate = self
+        pckCurrency.dataSource = self
+    }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
     }
 }

9015d18bbb65624df496ce74582e38f8b13ae6b7
omrobbie <putu.roby@yahoo.com>
2020-06-22 14:30:28 +0800

    login to firebase

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -14,5 +15,16 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text,
+            let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    return
+                }
+
+                self.performSegue(withIdentifier: "LoginToChat", sender: self)
+            }
+        }
     }
 }

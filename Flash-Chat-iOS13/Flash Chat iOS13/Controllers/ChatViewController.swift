394d7a80b5f182d91e4f8251447a994cf22b981b
omrobbie <putu.roby@yahoo.com>
2020-06-22 22:13:05 +0800

    order message by date

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -36,7 +36,7 @@ class ChatViewController: UIViewController {
     }
 
     func loadMessages() {
-        db.collection(Constants.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        db.collection(Constants.FStore.collectionName).order(by: Constants.FStore.dateField).addSnapshotListener { (querySnapshot, error) in
             if let error = error {
                 print(error.localizedDescription)
                 return
@@ -65,7 +65,8 @@ class ChatViewController: UIViewController {
             let messageSender = Auth.auth().currentUser?.email {
             db.collection(Constants.FStore.collectionName).addDocument(data: [
                 Constants.FStore.senderField: messageSender,
-                Constants.FStore.bodyField: messageBody
+                Constants.FStore.bodyField: messageBody,
+                Constants.FStore.dateField: Date().timeIntervalSince1970
                 ]
             ) { error in
                 if let error = error {

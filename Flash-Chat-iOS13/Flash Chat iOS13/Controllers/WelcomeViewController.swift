9d5e70732403ec8b6bba88012aaf304482e8e7f3
omrobbie <putu.roby@yahoo.com>
2020-06-23 14:37:17 +0800

    show hide navigation bar

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -26,4 +26,14 @@ class WelcomeViewController: UIViewController {
             charIndex += 1
         }
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
 }

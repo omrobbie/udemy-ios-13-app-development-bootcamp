ed4211eab41b53a8b6ee5fba0092c1e61e65772a
omrobbie <putu.roby@yahoo.com>
2020-06-26 20:27:42 +0800

    create new view for detail info

---
+++ H4X0R-News/H4X0R-News/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  H4X0R-News
+//
+//  Created by omrobbie on 26/06/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}

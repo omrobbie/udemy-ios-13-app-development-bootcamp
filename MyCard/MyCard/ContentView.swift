ad36493437d2351230ab349426ad0d9e624e9fbc
omrobbie <putu.roby@yahoo.com>
2020-06-26 15:12:02 +0800

    add new font and use it to text

--- MyCard/MyCard/ContentView.swift
+++ MyCard/MyCard/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
             Color(red: 0.09, green: 0.63, blue: 0.52)
                 .edgesIgnoringSafeArea(.all)
             Text("omrobbie")
-                .font(.title)
+                .font(Font.custom("Pacifico-Regular", size: 40))
                 .bold()
                 .foregroundColor(.white)
         }

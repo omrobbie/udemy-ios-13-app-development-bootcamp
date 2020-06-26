6243050a48ebb7a2a8b35bcc8ffc5f67cef12f22
omrobbie <putu.roby@yahoo.com>
2020-06-26 15:00:57 +0800

    * start my card project *

---
+++ MyCard/MyCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCard
+//
+//  Created by omrobbie on 26/06/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

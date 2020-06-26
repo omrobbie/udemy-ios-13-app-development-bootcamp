3143b5adb4d0603e00b4cbc789f8bcb15e09b8da
omrobbie <putu.roby@yahoo.com>
2020-06-26 20:49:02 +0800

    load url to web view

--- H4X0R-News/H4X0R-News/Views/DetailView.swift
+++ H4X0R-News/H4X0R-News/Views/DetailView.swift
@@ -7,18 +7,37 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct DetailView: View {
 
     let url: String?
 
     var body: some View {
-        Text("Hello, World!")
+        WebView(urlString: url)
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(url: "https://www.google.com")
+    }
+}
+
+struct WebView: UIViewRepresentable {
+
+    let urlString: String?
+
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
     }
 }

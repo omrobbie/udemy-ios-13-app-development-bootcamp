b9ad3d93ed06eaab3d3f8258c620d96dc388cf91
omrobbie <putu.roby@yahoo.com>
2020-06-26 20:53:22 +0800

    separate webview struct

---
+++ H4X0R-News/H4X0R-News/Views/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  WebView.swift
+//  H4X0R-News
+//
+//  Created by omrobbie on 26/06/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+
+    let urlString: String?
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
+

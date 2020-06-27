fa36df3e8dff4ec3c3a9b01b63f7754bb87298fa
omrobbie <putu.roby@yahoo.com>
2020-06-27 10:00:56 +0800

    load data to table view

--- Todoey-iOS13/Todoey/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/TodoListViewController.swift
@@ -10,7 +10,19 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
     }
 }

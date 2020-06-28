4126da1869b732f2bdd9ba6ccd780e9fa8fd751e
omrobbie <putu.roby@yahoo.com>
2020-06-28 12:16:40 +0800

    load item list based on category name

--- Todoey-iOS13/Todoey/Controller/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/Controller/TodoListViewController.swift
@@ -82,10 +82,9 @@ class TodoListViewController: UITableViewController, UISearchBarDelegate {
         let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
 
-        request.predicate = predicate
         request.sortDescriptors = [sortDescriptor]
 
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
     }
 
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
@@ -107,6 +106,7 @@ class TodoListViewController: UITableViewController, UISearchBarDelegate {
                 let newItem = Item(context: self.context)
                 newItem.title = text
                 newItem.done = false
+                newItem.parentCategory = self.selectedCategory
 
                 self.itemArray.append(newItem)
                 self.saveItems()
@@ -148,7 +148,16 @@ class TodoListViewController: UITableViewController, UISearchBarDelegate {
 //        }
     }
 
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+
+        if let predicate = predicate {
+            let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, predicate])
+            request.predicate = compoundPredicate
+        } else {
+            request.predicate = categoryPredicate
+        }
+
         do {
             itemArray = try context.fetch(request)
         } catch {

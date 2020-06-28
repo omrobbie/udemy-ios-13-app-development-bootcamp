64d3c320cab8c7ff6d91e674c02ede16cf9d09a5
omrobbie <putu.roby@yahoo.com>
2020-06-28 10:14:38 +0800

    add new table view controller with dummy data

---
+++ Todoey-iOS13/Todoey/Controller/CategoryViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by omrobbie on 28/06/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = "Item \(indexPath.row)"
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItem", sender: self)
+    }
+
+    @IBAction func btnAddTapped(_ sender: Any) {
+    }
+}

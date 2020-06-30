5fe2f430eaed2bb3f1f5316e3346fd619be52cb0
omrobbie <putu.roby@yahoo.com>
2020-06-30 12:19:05 +0800

    show normal quotes to list

--- InspoQuotes-iOS13/InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes-iOS13/InspoQuotes/QuoteTableViewController.swift
@@ -32,12 +32,15 @@ class QuoteTableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quotesToShow.count
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell")!
+        cell.textLabel?.text = quotesToShow[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
+        return cell
     }
 
     @IBAction func restorePressed(_ sender: UIBarButtonItem) {

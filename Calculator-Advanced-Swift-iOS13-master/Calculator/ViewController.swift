87e9954cc8129406a0ee2bcaf5f8b86c4e2153c4
omrobbie <putu.roby@yahoo.com>
2020-07-08 00:37:17 +0800

    type a number

--- Calculator-Advanced-Swift-iOS13-master/Calculator/ViewController.swift
+++ Calculator-Advanced-Swift-iOS13-master/Calculator/ViewController.swift
@@ -12,10 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
 
+    var isFinishedTypingNumber: Bool = true
+
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
     }
 
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text! += numValue
+            }
+        }
     }
-
 }

012ece0fb2895fbe5f93adfc27433ceb6b294988
omrobbie <putu.roby@yahoo.com>
2020-07-07 22:50:01 +0800

    clean project

--- Calculator-Advanced-Swift-iOS13-master/Calculator/ViewController.swift
+++ Calculator-Advanced-Swift-iOS13-master/Calculator/ViewController.swift
@@ -9,23 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var displayLabel: UILabel!
-    
-    
-    
+
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
-    
     }
 
-    
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
-    
     }
 
 }
-

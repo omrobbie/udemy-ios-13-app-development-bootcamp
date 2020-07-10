2dcabcfed080afa04d2b300e3ff13bd1759dbb1e
omrobbie <putu.roby@yahoo.com>
2020-07-10 15:29:11 +0800

    refactor: update to MVC structure

---
+++ Calculator-Advanced-Swift-iOS13-master/Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by omrobbie on 10/07/20.
+//  Copyright © 2020 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorLogic {
+
+    
+}

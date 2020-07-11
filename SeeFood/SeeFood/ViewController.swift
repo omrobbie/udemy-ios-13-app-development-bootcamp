079eda59b01d0014512546195602806aee924643
omrobbie <putu.roby@yahoo.com>
2020-07-11 10:55:57 +0800

    detect image with core ml and vision

--- SeeFood/SeeFood/ViewController.swift
+++ SeeFood/SeeFood/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
@@ -35,6 +37,36 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imgCamera.image = userPickedImage
+
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage into CIImage.")
+            }
+
+            detect(image: ciimage)
+        }
+
+        imgPicker.dismiss(animated: true)
+    }
+
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model failed!")
+        }
+
+        let request = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+
+            print(results)
+        })
+
+        let handler = VNImageRequestHandler(ciImage: image, options: [:])
+
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error.localizedDescription)
         }
     }
 }

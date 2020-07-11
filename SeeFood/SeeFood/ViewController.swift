79bf739005b782b817a4e83a854704fdc0345e29
omrobbie <putu.roby@yahoo.com>
2020-07-11 10:25:51 +0800

    setup project layout

--- SeeFood/SeeFood/ViewController.swift
+++ SeeFood/SeeFood/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgCamera: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func btnCameraTapped(_ sender: Any) {
     }
 }
